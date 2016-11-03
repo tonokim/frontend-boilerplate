f8d22b1f9066bfd41dec1f0257dd8b6ee9893e40
虞章君 <tonokim@qq.com>
2016-11-03 16:14:48 +0800

    code uglify css compress and extract single file add scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var rucksack = require('rucksack-css')
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path')
 
-module.exports = {
+var config = {
   context: path.join(__dirname, './client'),
   entry: {
     jsx: './index.js',
@@ -41,6 +43,16 @@ module.exports = {
         loader: 'style!css'
       },
       {
+        test: /\.scss$/,
+        include: /client/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loaders: [
@@ -48,6 +60,8 @@ module.exports = {
           'babel-loader'
         ]
       },
+      { test: /\.(png|jpg|jpeg|gif)$/, loader: 'url?limit=10000&name=./images/[name].[ext]' },
+      { test: /\.(ttf|eot|woff|woff2|otf|svg)/, loader: 'file?name=./font/[name].[ext]' }
     ],
   },
   resolve: {
@@ -69,3 +83,54 @@ module.exports = {
     hot: true
   }
 }
+
+if(process.env.NODE_ENV == 'production'){
+  config.output.filename = '[chunkhash:8].bundle.js';
+  config.plugins = [
+    new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+    new webpack.optimize.OccurenceOrderPlugin(),//按引用频度来排序 ID，以便达到减少文件大小的效果
+    new webpack.optimize.DedupePlugin(),  //查找相等或近似的模块，避免在最终生成的文件中出现重复的模块。
+		new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true, // React doesn't support IE8
+        warnings: false,
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', '[chunkhash:8].vendor.js'),
+    new ExtractTextPlugin('[chunkhash:8].bundle.css'),
+    new HtmlWebpackPlugin({
+      title: 'react-starter',
+      template: 'index.ejs',
+      inject: 'body'
+    })
+  ];
+  config.module.loaders[1] = {
+    test: /\.css$/,
+    include: /client/,
+    loader: ExtractTextPlugin.extract('style','css?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]!postcss')
+  }
+  config.module.loaders[2] = {
+    test: /\.css$/,
+    exclude: /client/,
+    loader: ExtractTextPlugin.extract('style','css')
+  }
+  config.module.loaders[3] = {
+    test: /\.scss$/,
+    include: /client/,
+    loader: ExtractTextPlugin.extract('style','css?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]!postcss!sass')
+  }
+}
+
+
+module.exports = config;
